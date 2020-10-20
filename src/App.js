3db8649efa57586b43c41cd2a5edb16c319cd2d6
Aleksi134 <aleleskinen11@gmail.com>
2020-10-20 14:49:41 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  let taskit = ["jutska 1", "jutska 2", "jutska 3"]
+  
+  const [uusiTaski, paivitaUusiTaski] = useState("jtn")
+  
+  function lisaaTaski() {
+    //tee jtn
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>varmistusheippa</p>
+      <input type="text" value={uusiTaski} />
+      <input type="button" value="Lisää taski" onClick={() => lisaaTaski() } />
+      <ul>
+        { taskit.map(yksittainenTaski => <li>{yksittainenTaski}</li> )}
+      </ul>
+     
     </div>
   );
 }
